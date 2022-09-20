49e71f6f2dd38138c8d5b17142301025c329f858
Noah Lundquist <nalundquist@gmail.com>
2022-09-20 11:02:39 -0700

    fulfill first test; add second
    Co-authored-by: Ted Gold <ted.jonathan.gold@gmail.com>

--- TTracker.Test/Models.Test/TTracker.Test.cs
+++ TTracker.Test/Models.Test/TTracker.Test.cs
@@ -7,11 +7,17 @@ namespace Tracker.Triangle.Test
 	public class TriangleTests
 	{
 		[TestMethod]
-    public void Triangle_HoldsThreeValues_True()
+    public void Triangle_IsTriangle_True()
     {
       Triangle testTriangle = new Triangle(3, 5, 8);
-      // is largest side > other sides combined
       Assert.AreEqual(true, testTriangle.IsTriangle(testTriangle.Side1, testTriangle.Side2, testTriangle.Side3));
     }
+
+		[TestMethod]
+		public void Triangle_IsEquilateral_True()
+		{
+			Triangle testTriangle = new Triangle(5, 5, 5);
+			Assert.AreEqual("equilateral", testTriangle.TriangleType(testTriangle.Side1, testTriangle.Side2, testTriangle.Side3));
+		}
 	}
 }
